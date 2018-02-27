b9c14a1e1c9e7fac45dd554d54454fa28ecc1394
lifeng <lifeng.kow@bizcube.com.sg>
2018-02-27 15:22:10 +0800

    package details get details, extend function, new transaction function, get transaction listing added

--- src/assets/js/single-page/packageDetails.js
+++ src/assets/js/single-page/packageDetails.js
@@ -2,20 +2,10 @@ var access=false;
 
 $(function(){
 
-  $("#packageTransactionAddForm #assurancePlus").change(function(){
-    if ($("#packageTransactionAddForm #assurancePlus").is(':checked')){
-      $("#packageTransactionAddForm #assurancePlusNo").show();
-    }else{
-      $("#packageTransactionAddForm #assurancePlusNo").hide();
-    }
-  });
-
   $("#packageTransactionAddForm #tranType").change(function(){
     if ($("#packageTransactionAddForm #tranType").val()=='Debit'){
       $("#packageTransactionAddForm .hoursDiv").show();
       $("#packageTransactionAddForm #assurancePlusNo").val("");
-      $("#packageTransactionAddForm #assurancePlus").prop('checked', false);
-      $("#packageTransactionAddForm #assurancePlusNo").hide();
       $("#packageTransactionAddForm .assurancePlusDiv").hide();
     }else if ($("#packageTransactionAddForm #tranType").val()=='Credit'){
       $("#packageTransactionAddForm .assurancePlusDiv").show();
@@ -24,18 +14,16 @@ $(function(){
     }else{
       $("#packageTransactionAddForm #hours").val("");
       $("#packageTransactionAddForm #assurancePlusNo").val("");
-      $("#packageTransactionAddForm #assurancePlus").prop('checked', false);
-      $("#packageTransactionAddForm #assurancePlusNo").hide();
       $("#packageTransactionAddForm .hoursDiv").hide();
       $("#packageTransactionAddForm .assurancePlusDiv").hide();
     }
   });
 
   //get packageID from url
-  /*var urlParams = new URLSearchParams(window.location.search),
+  var urlParams = new URLSearchParams(window.location.search),
       packageID = urlParams.get('packageID');
 
-  var checkAccess =
+  /*var checkAccess =
     $.ajax({
       url: apiSrc+"BCMain/iCtc1.CheckIsAdmin.json",
       method: "POST",
@@ -59,7 +47,7 @@ $(function(){
     }else{
       $('.dataAccess').show();
     }
-  });
+  });*/
 
   getPackageDetails(packageID);
   getPackageTransaction(packageID);
@@ -67,27 +55,77 @@ $(function(){
   //add transaction
   $('#packageTransactionAddForm #submit').click(function(){
     addNewtransaction(packageID, '');
-  });*/
+  });
+
+  $('#packageExtendForm #submit').click(function(){
+    extendExpiry(packageID);
+  });
 });
 
 function addNewtransaction(PackageID, CaseID){
-  var Type, ManDays, Remarks;
+  var Type, Hours, AssuranceNo, Remarks;
   TranType =  $('#packageTransactionAddForm #tranType').val();
-  Days = $('#packageTransactionAddForm #days').val();
+  AssuranceNo = $('#packageTransactionAddForm #assurancePlusNo').val()*8;
+  Hours = $('#packageTransactionAddForm #hours').val();
   Remarks = $('#packageTransactionAddForm #remarks').val();
 
-  if (TranType.length==0 || Days.length==0 || Remarks.length==0){
+  if (TranType.length==0 || Remarks.length==0){
     alert('Please fill in all mandatory fields!');
     return false;
   }
-  if (IsValidManDay(Days)==false){
-    alert('Please fill day in whole/decimal .5 only!');
+
+  if (TranType=='Credit' && AssuranceNo==''){
+    alert('Please fill in all mandatory fields!');
+    return false;
+  } else if (TranType=='Debit' && Hours.length==0){
+    alert('Please fill in all mandatory fields!');
     return false;
   }
 
-  var data = {'PackageID':PackageID, 'CaseID':CaseID, 'TranType':TranType, 'Days':Days, 'Remarks': Remarks};
+  if (AssuranceNo!=''){
+    Hours = AssuranceNo;
+  }else{
+    Hours = Hours;
+  }
+
+  var data = {'PackageID':PackageID, 'FLID':CaseID, 'TranType':TranType, 'Hours':Hours, 'Remarks': Remarks};
   $.ajax({
     url: apiSrc+"BCMain/Ctc1.AddNewPackageTransactions.json",
+    method: "POST",
+    dataType: "json",
+    xhrFields: {withCredentials: true},
+    data: { 'data':JSON.stringify(data),
+            'WebPartKey':WebPartVal,
+            'ReqGUID': getGUID() },
+    success: function(data){
+      if ((data) && (data.d.RetVal === -1)) {
+        if (data.d.RetData.Tbl.Rows.length > 0) {
+          if (data.d.RetData.Tbl.Rows[0].Success == true) {
+            getPackageDetails(PackageID);
+            getPackageTransaction(PackageID);
+          } else { alert(data.d.RetData.Tbl.Rows[0].ReturnMsg); }
+        }
+      }
+      else {
+        alert(data.d.RetMsg);
+      }
+    }
+  });
+}
+
+function extendExpiry(PackageID){
+  var NewExpiry, Remarks;
+  NewExpiry =  $('#packageExtendForm #expiryDate').val();
+  Remarks = $('#packageExtendForm #remarks').val();
+
+  if (NewExpiry.length==0 || Remarks.length==0){
+    alert('Please fill in all mandatory fields!');
+    return false;
+  }
+
+  var data = {'PackageID':PackageID, 'TranType':'Extend', 'NewExpiry':NewExpiry, 'Remarks': Remarks};
+  $.ajax({
+    url: apiSrc+"BCMain/Ctc1.ExtendPackageExpiry.json",
     method: "POST",
     dataType: "json",
     xhrFields: {withCredentials: true},
@@ -124,15 +162,20 @@ function getPackageDetails(PackageID){
       if ((data) && (data.d.RetVal === -1)) {
         if (data.d.RetData.Tbl.Rows.length > 0) {
           var packageDetails = data.d.RetData.Tbl.Rows[0];
-
+          var assuranceFlag;
+          if (packageDetails.AssurancePlus==1){
+            assuranceFlag = 'Yes'
+          }else{
+            assuranceFlag = 'No';
+          }
           var packageDate = convertDateTime(packageDetails.CreatedDate,'datetime'),
               startDate = convertDateTime(packageDetails.StartDate,'date'),
               expiryDate = convertDateTime(packageDetails.ExpiryDate,'date');
           $('#summary .organization').html(packageDetails.Organization);
-          $('#summary .packageType').html(packageDetails.PackageType);
           $('#summary .product').html(packageDetails.Product);
           $('#summary .status').html(packageDetails.Status);
-          $('#summary .manDays').html(packageDetails.ManDaysLeft+'/'+packageDetails.ManDaysBought);
+          $('#summary .packageType').html(packageDetails.PackageType);
+          $('#summary .assurancePlusLbl').html(assuranceFlag);
           $('#summary .startDate').html(startDate);
           $('#summary .expiryDate').html(expiryDate);
           $('#summary .pkgCreatedBy').html(packageDetails.CreatedBy);
@@ -161,7 +204,7 @@ function getPackageTransaction(PackageID){
           var htmlString = '';
           for (var i=0; i<transactionDetails.length; i++ ){
             var tranDate = convertDateTime(transactionDetails[i].TranDate,'date');
-            htmlString += '<tr id="'+ transactionDetails[i].PackageID  +'"> <td>'+transactionDetails[i].TranType+'</td> <td>'+transactionDetails[i].Days+'</td> <td>'+tranDate+'</td> <td>'+transactionDetails[i].TranCreatedBy+'</td> <td>'+transactionDetails[i].Remarks+'</td> <td> <a href="./case.html?caseID='+transactionDetails[i].CaseID+'"">'+transactionDetails[i].CaseID+'</a> </td> </tr>';
+            htmlString += '<tr id="'+ transactionDetails[i].PackageID  +'"> <td>'+tranDate+'</td> <td>'+transactionDetails[i].TranCreatedBy+'</td> <td>'+transactionDetails[i].Hours+'</td> <td>'+transactionDetails[i].Remarks+'</td> </tr>';
           }
           $('.packagetranTable tbody').html(htmlString);
         }
@@ -169,11 +212,6 @@ function getPackageTransaction(PackageID){
     }
   });
 };
-
-function IsValidManDay(ManDay) {
-	var re = /^\d*(\.[05])?$/;
-	return re.test(ManDay);
-}
 
 function convertDateTime(inputFormat, type) {
   if (inputFormat == null){
@@ -188,33 +226,6 @@ function convertDateTime(inputFormat, type) {
   }else if (type == 'time'){
     return [pad(d.getHours()), pad(d.getMinutes()), pad(d.getSeconds())].join(':');
   }
-};
-
-//geneare drop down optioms
-function GetDropdownList(id, category) {
-  var data = {'LookupCat': category}
-  $.ajax({
-    url: apiSrc+"BCMain/iCtc1.Lookup_Get.json",
-    method: "POST",
-    dataType: "json",
-    xhrFields: {withCredentials: true},
-    data: { 'data': JSON.stringify(data),
-            'WebPartKey':WebPartVal,
-            'ReqGUID': getGUID() },
-    success: function(data){
-      if ((data) && (data.d.RetVal === -1)) {
-        if (data.d.RetData.Tbl.Rows.length > 0) {
-          var lookup = data.d.RetData.Tbl.Rows;
-          for (var i=0; i<lookup.length; i++ ){
-            $(id).append('<option value="'+lookup[i].LookupKey+'">'+lookup[i].Description+'</option>');
-          }
-        }
-      }
-      else {
-        alert(data.d.RetMsg);
-      }
-    }
-  });
 };
 
 function getGUID() {
